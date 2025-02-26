81690220e57ad0b44380e7bd88313c809a729d00
Paigomanh <rahulrahulsajeevan007@gmail.com>
2025-02-26 15:05:25 +0530

    updates

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+// Import this to make className work
+import 'nativewind';
+import "./global.css";
+
+export default function App() {
+  return (
+    <View className="mt-[5%]">
+      <Text className="mt-5">Hi I'm Rahul</Text>
+    </View>
+  );
+}

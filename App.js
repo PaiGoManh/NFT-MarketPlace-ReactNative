5c882d6c87df61ec0bd1d391e7928d9b23c9b8f5
Paigomanh <rahulrahulsajeevan007@gmail.com>
2025-02-26 14:39:05 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import './App.css'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hi Im Rahul</Text>
+    </View>
+  );
+}
